2f7fd3d49be755a64e9ea99d6ca57d507f5766d7
rudynakodach <107307636+rudynakodach@users.noreply.github.com>
2022-06-21 11:30:29 +0200

    added SkyblockSniper

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import time
 import requests
 from requests import *
@@ -5,45 +6,118 @@ import PySimpleGUI as sg
 import json
 import webbrowser
 import os
+from pathlib import Path
 import pyautogui
 import cv2
 from PIL import ImageGrab
 import numpy as np
 import random
+from colorama import Fore, Back, Style
+import colorama
+import base64
+import asyncio
+from timeit import default_timer
+import re
+import pandas as pd
+from concurrent.futures import ThreadPoolExecutor
+op = os.name == 'nt'
+if op: import winsound
+from plyer import notification
 
-os.system("cls")
 
-print("Starting INIT phase...")
 
-chrome_path = "C:\Program Files\Google\Chrome\Application\chrome.exe"
-webbrowser.register('google-chrome', None, webbrowser.BackgroundBrowser(chrome_path))
+colorama.init(autoreset=True) #reset colorama after each print
 
+os.system("cls") #flush the console
+
+print(f"{Fore.LIGHTBLUE_EX}Starting INIT phase...")
+
+chrome_path = "C:\Program Files\Google\Chrome\Application\chrome.exe" # assing chrome path
+webbrowser.register('google-chrome', None, webbrowser.BackgroundBrowser(chrome_path)) # register chrome as the browser to use
+
+path = Path(__file__).parent.absolute() # get current file path
+
+# ###################################################################################################################### init/data folder
 
 try:
-    print("[+] Starting...")
-    with open('apikey.txt', 'r') as f:
-        print("[+] API Key found and read!")
+    print(f"{Fore.LIGHTBLUE_EX}[INIT] Checking for data folder...")
+    os.makedirs(path / "data")
+    print(f"{Fore.LIGHTBLUE_EX}[INIT] Data folder created!")
+except FileExistsError:
+    print(f"{Fore.LIGHTBLUE_EX}[INIT] Data folder already exists")
+
+# ###################################################################################################################### config.json file creation
+
+if not os.path.exists(path / "data" / "config.json"):
+    print(f"{Fore.CYAN}[SETUP] Creating config file...")
+    open(path / "data" / "config.json", "w").write(json.dumps({"debug": "False"}, indent=4))
+else:
+    print(f"{Fore.LIGHTGREEN_EX}[DEBUG] Config file already exists!")
+
+# ###################################################################################################################### API key
+
+try:
+    print(f"{Fore.LIGHTGREEN_EX}[+]{Fore.WHITE} Getting your API key...")
+    with open(f"{path}/data/apikey.dat", 'r') as f:
+        print(f"{Fore.LIGHTGREEN_EX}[+]{Fore.WHITE} API Key found!")
         apikey = f.read()
+        #apikey = base64.b64decode(apikey).decode('utf-8')
+        print(f"{Fore.LIGHTGREEN_EX}[+]{Fore.WHITE} API Key decoded!")
         apikey = str(apikey)
-except:
-    print("[!] Error: No API key found!")
-    with open('apikey.txt','w+') as f:
-        print("[+] Creating API-Key file...")
-        print("[+] Done!")
-        print('[!] Please enter your API key:')
+except FileNotFoundError:
+    print(f"{Fore.RED}[!] Error{Fore.WHITE}: No API key found!")
+    print(f"{Fore.LIGHTGREEN_EX}[+]{Fore.WHITE} Creating API key file...")
+    with open(f'{path}/data/apikey.dat','w') as f:
+        print(f"{Fore.LIGHTGREEN_EX}[+]{Fore.WHITE} Done!")
+        print(f'{Fore.RED}[!]{Fore.WHITE} Please enter your API key!')
         apikey = input("Enter your API key: ")
+        print(f"{Fore.LIGHTRED_EX}Encoding API key...")
+        #apikey = base64.b64encode(apikey.encode('utf-8'))
+        apikey = str(apikey)
+        print(f"{Fore.LIGHTRED_EX} API Key encoded!")
+
         f.write(apikey)
         apikey = str(apikey)
+        f.close()
+        print(f"{Fore.LIGHTGREEN_EX}[+]{Fore.WHITE} API Key saved!")
 
+# ###################################################################################################################### reading config.json file
 
+with open(path / "data" / "config.json", "r") as f:
+    data = json.load(f)
+    debug = data["debug"]
 
-print(f"Your API Key - {apikey}")
+# ###################################################################################################################### general debug files
 
-#apikey = "148671b2-ef98-426d-907d-58fca94b720a"
+if debug == "True":
+    print(f"{Fore.LIGHTGREEN_EX}[+]{Fore.RESET} Preparing debug files...")
+    if not os.path.exists(path / "data" / "debug"):
+        os.makedirs(path / "data" / "debug")
+        print(f"{Fore.LIGHTGREEN_EX}[+]{Fore.RESET} Debug folder created!")
+    else:
+        print(f"{Fore.LIGHTGREEN_EX}[+]{Fore.RESET} Debug folder already exists!")
+    print(f"{Fore.LIGHTGREEN_EX}[+]{Fore.RESET} Preparing log files...")
+    if not os.path.exists(path / "data" / "debug" / "log"):
+        os.makedirs(path / "data" / "debug" / "log")
+        print(f"{Fore.LIGHTGREEN_EX}[+]{Fore.RESET} Log folder created!")
+    else:
+        print(f"{Fore.LIGHTGREEN_EX}[+]{Fore.RESET} Log folder already exists!")
+    if not os.path.exists(path / "data" / "debug" / "log" / "debug-log.log"):
+        with open('debug-log.log', 'w+') as f:
+            print(f"{Fore.LIGHTGREEN_EX}[+]{Fore.RESET} Log file created!")
+            f.close()
+    else:
+        print(f"{Fore.LIGHTGREEN_EX}[+]{Fore.RESET} Log file already exists!")
+
+print(f"{Fore.LIGHTGREEN_EX}[DEBUG] Debug mode is set to {debug}")
+apikey = str(apikey)
+
+print(f"Your API Key - {Fore.YELLOW}{apikey}")
+
 
 mainLayout = [
     [sg.Text("Wybierz tryb")],
-    [sg.Combo(['Material calculator','Command macros','Quick Wiki','StatTrak','Igor'],key='-MODE-',default_value='Material calculator',size=(20,1))],
+    [sg.Combo(['Material calculator','Command macros','Quick Wiki','Mr Fisher','SkyblockSniper'],key='-MODE-',default_value='Material calculator',size=(20,1))],
     [sg.Text("")],
     [sg.Button("Run",key='-RUN-')]
 ]
@@ -98,7 +172,7 @@ statLayout = [
 igorLayout = [
     [sg.Text("")],
     [sg.Text("WYŁĄCZ WSZYSTKIE TEXTUREPACKI!!!")],
-    [sg.Button("Start",key='-START-'),sg.Button("STOP",key='-STOP-')]
+    [sg.Button("Start",key='-START-'),sg.Button("STOP",key='-STOP-'),sg.Button("Info",key='-INFO-')],
 ]
 
 
@@ -285,7 +359,7 @@ while True:
                         pyautogui.press('enter')
 
                 else:
-                    print("[ERROR] Macros are not enabled")
+                    print("{Fore.RED}[ERROR]{Fore.WHITE} Macros are not enabled")
 
                 if event == '-BACK-':
                     macroWindow.close()
@@ -335,33 +409,31 @@ while True:
                         print(data)
 
                     elif get(f"https://api.mojang.com/users/profiles/minecraft/{nick}").status_code != 200:
-                        print(f"[ERROR] Nick not found")
+                        print(f"{Fore.RED}[ERROR]{Fore.WHITE} Nickname not found!")
                     else:
-                        print(f"[ERROR] Something went wrong")
+                        print(f"{Fore.RED}[ERROR]{Fore.WHITE} Something went wrong!")
 
 
             
         elif mode == 'Igor':
+            mainWindow.close()
             igorWindow = sg.Window("SkyBlock UnAddons", igorLayout, keep_on_top=True,element_justification='center',finalize=True)
             while True:
                 event, values = igorWindow.read()
                 if event == '-START-':
                     igorWindow.close()
-                    print("Starting 'Igor' Fishing Bot.")
+                    print("Starting the Fishing Bot.")
                     time.sleep(0.5)
-                    
-
-
                     def initializePyAutoGUI():
                         print("Initializing PyAutoGUI...")
                         pyautogui.FAILSAFE = True
                         print("Initializing PyAutoGUI... Done!")
                     def take_capture(magnification):
                         mx, my = pyautogui.position()  
-                        x = mx - 5  
-                        y = my - 5
+                        x = mx - 10  
+                        y = my - 10
                         capture = ImageGrab.grab(
-                            bbox=(x, y, x + 20, y + 20)
+                            bbox=(x, y, x + 30, y + 30)
                         )  
                         arr = np.array(capture)
                         res = cv2.cvtColor(
@@ -375,33 +447,167 @@ while True:
                             )
                         return res
                     def autofish(tick_interval, threshold, magnification):
+                        mx, my = pyautogui.position()
+                        pyautogui.moveTo(mx, my+5)
+
                         print("Casting the fishing line...")
                         pyautogui.rightClick()
-                        time.sleep(2)  
-                        img = take_capture(magnification)  
+                        time.sleep(0.05)
+                        pyautogui.moveTo(mx,my-5)
+                        time.sleep(2)
                         print("Taking initial capture...")
+                        img = take_capture(magnification)
                         print("Showing converted image...")
-                        while np.sum(img == 0) > threshold:  
+                        while np.sum(img == 0) > threshold:
                             img = take_capture(magnification)
                             time.sleep(tick_interval)
                             cv2.imshow('window', img)
                             if cv2.waitKey(25) & 0xFF == ord('q'):
+                                print("Destroying all windows...")
                                 cv2.destroyAllWindows()
+                                print(f"{Fore.GREEN}[!]{Fore.RESET} Done!")
+                                print(f"{Fore.YELLOW}[-]{Fore.RESET} Exiting...")
                                 break
                         pyautogui.rightClick()
                         time.sleep(1)
-                    def main():
+                    def Fishmain():
                         print("Starting...")
                         initializePyAutoGUI()
-                        time.sleep(5)  
+                        time.sleep(5)
                         i = 0
                         while i < 1000:
-                            autofish(0.01, 0, 5)
-                            print(f"Found someting! Fisehd {i} items so far. Continuing...")
+                            autofish(0.01, 0, 4)
+                            posx, posy = pyautogui.position()
+                            pyautogui.moveTo(posx-2,posy)
+                            time.sleep(0.05)
+                            pyautogui.moveTo(posx+2,posy)
                             i += 1
-                    main()
+                            time = datetime.now().strftime("%H:%M:%S")
+                            print(f"[{time}] Found someting! Fisehd {Fore.LIGHTCYAN_EX}{i}{Fore.RESET} items so far. Continuing...")
+                            if debug == "True":
+                                with open(f'{path}/data/log/debug-log.log','a') as log:
+                                    log.write(f"[{time}] Found someting! Fished {Fore.LIGHTCYAN_EX}{i}{Fore.RESET} items so far. \n{Fore.BLUE}Continuing...")
+                    Fishmain()
                 elif event == '-STOP-' or event == sg.WIN_CLOSED:
                     break
                 elif event == '-INFO-':
-                    print(f"Igor Fishing Bot v0.2")
-                    print(f"Aby łowić za pomocą sztucznej inteligencji, program musi wykrywać spławik.\nWyłącz FullScreena aby widziec okno OpenCV \nMINECRAFT MUSI BYĆ WIDOCZNY NA EKRANIE")
+                    print(f"Mr Fisherman Bot v0.2\nAby łowić za pomocą sztucznej inteligencji, program musi wykrywać spławik.\nWyłącz FullScreena aby widziec okno Open ComputerVision\nMINECRAFT MUSI BYĆ WIDOCZNY NA EKRANIE")
+        elif mode == 'SkyblockSniper':
+            mainWindow.close()
+            print(f"{Fore.GREEN}[!]{Fore.RESET} Starting Skyblock Sniper...")
+            print(f"Made by {Fore.BLUE} csjh{Fore.RESET}")  
+            c = requests.get("https://api.hypixel.net/skyblock/auctions?page=0")
+            resp = c.json()
+            now = resp['lastUpdated']
+            toppage = resp['totalPages']
+
+            results = []
+            prices = {}
+
+            # stuff to remove
+            REFORGES = [" ✦", "⚚ ", " ✪", "✪", "Stiff ", "Lucky ", "Jerry's ", "Dirty ", "Fabled ", "Suspicious ", "Gilded ", "Warped ", "Withered ", "Bulky ", "Stellar ", "Heated ", "Ambered ", "Fruitful ", "Magnetic ", "Fleet ", "Mithraic ", "Auspicious ", "Refined ", "Headstrong ", "Precise ", "Spiritual ", "Moil ", "Blessed ", "Toil ", "Bountiful ", "Candied ", "Submerged ", "Reinforced ", "Cubic ", "Warped ", "Undead ", "Ridiculous ", "Necrotic ", "Spiked ", "Jaded ", "Loving ", "Perfect ", "Renowned ", "Giant ", "Empowered ", "Ancient ", "Sweet ", "Silky ", "Bloody ", "Shaded ", "Gentle ", "Odd ", "Fast ", "Fair ", "Epic ", "Sharp ", "Heroic ", "Spicy ", "Legendary ", "Deadly ", "Fine ", "Grand ", "Hasty ", "Neat ", "Rapid ", "Unreal ", "Awkward ", "Rich ", "Clean ", "Fierce ", "Heavy ", "Light ", "Mythic ", "Pure ", "Smart ", "Titanic ", "Wise ", "Bizarre ", "Itchy ", "Ominous ", "Pleasant ", "Pretty ", "Shiny ", "Simple ", "Strange ", "Vivid ", "Godly ", "Demonic ", "Forceful ", "Hurtful ", "Keen ", "Strong ", "Superior ", "Unpleasant ", "Zealous "]
+
+            # Constant for the lowest priced item you want to be shown to you; feel free to change this
+            LOWEST_PRICE = 5
+
+            # Constant to turn on/off desktop notifications
+            NOTIFY = False
+
+            LOWEST_PERCENT_MARGIN = 1/2
+
+            START_TIME = default_timer()
+
+            def fetch(session, page):
+                global toppage
+                base_url = "https://api.hypixel.net/skyblock/auctions?page="
+                with session.get(base_url + page) as response:
+                    data = response.json()
+                    toppage = data['totalPages']
+                    if data['success']:
+                        toppage = data['totalPages']
+                        for auction in data['auctions']:
+                            if not auction['claimed'] and auction['bin'] == True and not "Furniture" in auction["item_lore"]:
+                                index = re.sub("\[[^\]]*\]", "", auction['item_name']) + auction['tier']
+                                for reforge in REFORGES: index = index.replace(reforge, "")
+                                if index in prices:
+                                    if prices[index][0] > auction['starting_bid']:
+                                        prices[index][1] = prices[index][0]
+                                        prices[index][0] = auction['starting_bid']
+                                    elif prices[index][1] > auction['starting_bid']:
+                                        prices[index][1] = auction['starting_bid']
+                                else:
+                                    prices[index] = [auction['starting_bid'], float("inf")]
+                                    
+                                if prices[index][1] > LOWEST_PRICE and prices[index][0]/prices[index][1] < LOWEST_PERCENT_MARGIN and auction['start']+60000 > now:
+                                    results.append([auction['uuid'], auction['item_name'], auction['starting_bid'], index])
+                    return data
+
+            async def get_data_asynchronous():
+                pages = [str(x) for x in range(toppage)]
+                with ThreadPoolExecutor(max_workers=10) as executor:
+                    with requests.Session() as session:
+                        loop = asyncio.get_event_loop()
+                        START_TIME = default_timer()
+                        tasks = [
+                            loop.run_in_executor(
+                                executor,
+                                fetch,
+                                *(session, page)
+                            )
+                            for page in pages if int(page) < toppage
+                        ]
+                        for response in await asyncio.gather(*tasks):
+                            pass
+
+            def SSmain():
+                global results, prices, START_TIME
+                START_TIME = default_timer()
+                results = []
+                prices = {}
+                
+                loop = asyncio.new_event_loop()
+                asyncio.set_event_loop(loop)
+                future = asyncio.ensure_future(get_data_asynchronous())
+                loop.run_until_complete(future)
+                
+                if len(results): results = [[entry, prices[entry[3]][1]] for entry in results if (entry[2] > LOWEST_PRICE and prices[entry[3]][1] != float('inf') and prices[entry[3]][0] == entry[2] and prices[entry[3]][0]/prices[entry[3]][1] < LOWEST_PERCENT_MARGIN)]
+                
+                if len(results): 
+
+                    if NOTIFY: 
+                        notification.notify(
+                            title = max(results, key=lambda entry:entry[1])[0][1],
+                            message = "Lowest BIN: " + f'{max(results, key=lambda entry:entry[1])[0][2]:,}' + "\nSecond Lowest: " + f'{max(results, key=lambda entry:entry[1])[1]:,}',
+                            app_icon = None,
+                            timeout = 4,
+                        )
+                    
+                    df=pd.DataFrame(['/viewauction ' + str(max(results, key=lambda entry:entry[1])[0][0])])
+                    df.to_clipboard(index=False,header=False) 
+                    
+                    done = default_timer() - START_TIME
+                    if op: winsound.Beep(700, 500)
+                    for result in results:
+                        print("Auction UUID: " + str(result[0][0]) + " | Item Name: " + str(result[0][1]) + " | Item price: {:,}".format(result[0][2]), " | Second lowest BIN: {:,}".format(result[1]) + " | Time to refresh AH: " + str(round(done, 2)))
+                    print("\nLooking for auctions...")
+
+            print("Looking for auctions...")
+            SSmain()
+
+            def dostuff():
+                global now, toppage
+
+                if time.time()*1000 > now + 60000:
+                    prevnow = now
+                    now = float('inf')
+                    c = requests.get("https://api.hypixel.net/skyblock/auctions?page=0").json()
+                    if c['lastUpdated'] != prevnow:
+                        now = c['lastUpdated']
+                        toppage = c['totalPages']
+                        SSmain()
+                    else:
+                        now = prevnow
+                time.sleep(0.25)
+
+            while True:
+                dostuff()
